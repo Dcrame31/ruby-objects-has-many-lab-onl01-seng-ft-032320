440580fbdf6d371a68975df7e5d8f6d5806d5c4c
Diane Crame <dcrame@icloud.com>
2020-04-06 18:59:39 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -12,6 +12,10 @@ class Post
   end
   
   def author_name
-    self.author? self.author.name : nil
+    if self.author == nil
+      return nil
+    else 
+      self.author.name
+    end
   end
 end
